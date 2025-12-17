a1d86e87fdb9cd244ef2cb5aed6738c5edc26a07
Key'n Brosdahl <keynb1999@gmail.com>
2025-12-17 16:36:49 -0600

    created Algorithms and Data Structure folder

--- Visualizer/src/App.jsx
+++ Visualizer/src/App.jsx
@@ -1,15 +1,15 @@
-import './App.css';
-import Bubble from './components/Sorting/BubbleSort/Bubble';
-import Navbar from './components/navbar/Navbar';
+import "./App.css";
+import Bubble from "./Algorithms/Sorting/BubbleSort/Bubble";
+import Navbar from "./components/navbar/Navbar";
 const App = () => {
   return (
-<>
-<Navbar />
-<div className="app-container">
-    <Bubble />
-</div>
-</>
-  )
-}
+    <>
+      <Navbar />
+      <div className="app-container">
+        <Bubble />
+      </div>
+    </>
+  );
+};
 
-export default App
+export default App;
